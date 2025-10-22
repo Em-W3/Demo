2cdff84968e2301a52882eb71b54d46f03908b30
Em-W3 <emilyjade140@gmail.com>
2025-10-22 22:27:20 +0100

    Fix API key variable and logging syntax
    Refactor API key retrieval and fix syntax errors.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 "javascript
   require('dotenv').config();
 
-Const apiKEY=process.env.API_KEY;
+Const apiKey=process.env.API_KEY;
 
 //Now you can use apiKey in your code
 Console.log(apiKey):
+"
+"javascript
+  const apiKey = 
+  document.querySelector('meta[name-"api-key"}').getAttribute('content');
+console.log(apiKey);
 "
