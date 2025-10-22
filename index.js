1d0ed6dfa1985c20a6722aa9b8648d221134aa2b
Em-W3 <emilyjade140@gmail.com>
2025-10-22 22:38:45 +0100

    Implement fetch for hardcover fiction list
    Add fetch request to retrieve current hardcover fiction list

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ Console.log(apiKey):
   document.querySelector('meta[name-"api-key"}').getAttribute('content');
 console.log(apiKey);
 "
+  "javascript
+  fetch('/lists/current/hardcover-fiction.json")
+        .then(response=>{
+          if (!response.ok) {
+            throw new Error('Network response was not ok '+ response.statusText);
+          }
+          return response.json();// Or response.text() if it's not JSON
+        })
+        .then(data=>{
+          // Do something with your data here console.log(data);
+         })
+        .catch(error=>{
+          console.error('There was a problem with the fetch operation:', error);
+        });
+        "
+          }
+        }])
