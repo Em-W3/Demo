b4638254a56c7ac92681b7115910dc3cdab96ea2
Em-W3 <emilyjade140@gmail.com>
2025-10-22 22:18:22 +0100

    Configure dotenv and log API key
    Added dotenv configuration and API key usage.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, world! you smell like poop");
+"javascript
+  require('dotenv').config();
+
+Const apiKEY=process.env.API_KEY;
+
+//Now you can use apiKey in your code
+Console.log(apiKey):
+"
